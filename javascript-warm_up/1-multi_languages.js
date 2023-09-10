a4bc7b5a4bed8d1d6c8c98a5907c51dd786bc78b
charlesnyanje <charleswilliam26@gmail.com>
2023-09-10 11:10:29 +0300

    1. 3 languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,9 @@
 
 
 const first_line = "C is fun";
-const second_line = "\nPython is cool";
-const third_line = "\nJavascript is amazing";
+const second_line = "Python is cool";
+const third_line = "Javascript is amazing";
 
-console.log(first_line,second_line,third_line);
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
