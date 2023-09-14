dee6aa1eb4268b2983f11f509efb028637ac9940
charlesnyanje <charleswilliam26@gmail.com>
2023-09-14 21:23:03 +0300

    2. Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
+const Rectangle = require("./0-rectangle");
+
 module.exports = class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)){
-        return {};
+        let myObj = {};
     }
     else{
         this.width = w;
@@ -12,3 +14,5 @@ module.exports = class Rectangle {
   }
   
 };
+
+x = new Rectangle()
