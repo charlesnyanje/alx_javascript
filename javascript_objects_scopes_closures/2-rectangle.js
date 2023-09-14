909e0e5677724940ee4a4f6d10d67d4c2b820230
charlesnyanje <charleswilliam26@gmail.com>
2023-09-14 19:00:07 +0300

    2. Rectangle #2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+const Rectangle = require("./0-rectangle");
+
+module.exports = class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  empty_obj() {
+    if (this.w || this.h == 0 || this.w || this.h < 0) {
+      const myObj = {};
+    }
+  }
+};
