9a1313af5365a8e90cc6bda5df3633fdcf9c13f1
charlesnyanje <charleswilliam26@gmail.com>
2023-09-16 21:10:59 +0300

    2. Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,19 @@
-const request = require('request');
+const request = require("request");
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = `${process.argv[3]}`;
-
-
-request.get(url, (err, response, body) => {
-  const filmsData = JSON.parse(body);
+function getCharacterID(characterId) {
+  const url = "https://swapi-api.alx-tools.com/api/films/";
+  request.get(url, (err, response, body) => {
+    const filmsData = JSON.parse(body);
 
     const filmsWithWedgeAntilles = filmsData.results.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      film.characters.includes(
+        `https://swapi-api.alx-tools.com/api/people/${characterId}/`
+      )
     );
 
-  
     console.log(`${filmsWithWedgeAntilles.length}`);
-  }
-);
+  });
+}
+const characterId = `${process.argv[3]}`;
+
+getCharacterID(characterId);
