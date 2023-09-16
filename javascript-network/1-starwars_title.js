840ead73ee66a5ba5a451446a68e064cbd6f5b64
charlesnyanje <charleswilliam26@gmail.com>
2023-09-16 15:46:43 +0300

    1. Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,13 +1,24 @@
 const request = require('request');
 
-const url = `${process.argv[3]}`
+// Function to get the title of a Star Wars movie by episode number
+function getMovieTitleByEpisode(id) {
+  const apiUrl = `https://swapi.dev/api/films/${id}`;
 
-const id = `${process.argv[2]}${url}`;
+  request(apiUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      console.log(`Title of Episode ${id}: ${movieData.title}`);
+    } else {
+      console.error(`Error fetching data: ${error}`);
+    }
+  });
+}
 
+// Usage: Provide the episode number as a command line argument
+const id = process.argv[2];
 
-
-request
-  .get(URL)
-  .on('response', function(response) {
-    console.log(`code: ${response.statusCode}`); // 200
-  })
+if (!id) {
+  console.error('Please provide an episode number as an argument.');
+} else {
+  getMovieTitleByEpisode(id);
+}
