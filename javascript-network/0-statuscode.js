7e3834e28216e2bdfc3392b03c8cf880d800cd6f
charlesnyanje <charleswilliam26@gmail.com>
2023-09-16 09:58:24 +0300

    0. Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,11 +1,7 @@
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.log('<URL>');
-  process.exit(1);
-}
 
-const url = process.argv[2];
+const url = process.argv[1];
 
 request(url, (error, response) => {
   if (error) {
