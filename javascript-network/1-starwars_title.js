c9a6288d9f5515135cb5951945d4f78ea86d8817
charlesnyanje <charleswilliam26@gmail.com>
2023-09-16 16:14:55 +0300

    1. Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 function getMovieTitleByID(id) {
-  const url = `https://swapi.dev/api/films/${id}`;
+  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
   request.get(url, (err, response, body) => {
     const movieData = JSON.parse(body);
     console.log(`${movieData.title}`);
