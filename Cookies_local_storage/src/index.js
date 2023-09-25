bad434e66f51492324c3907c0cae2c9ec1b5a2ad
charlesnyanje <charleswilliam26@gmail.com>
2023-09-25 17:58:12 +0300

    2. Read cookie

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,20 +3,33 @@ function setCookies() {
   const emailInput = document.getElementById("email").value;
 
   const expirationDate = new Date(Date.now() + 10 * 24 * 60 * 60 * 1000);
-  
+
   const expires = `expires=${expirationDate.toUTCString()}`;
 
-  
   document.cookie = `firstname=${firstnameInput}; ${expires}; path=/`;
   document.cookie = `email=${emailInput}; ${expires}; path=/`;
 }
 
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split("=");
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+  return "";
+}
+
 function showCookies() {
   const cookies = document.cookie.split(";");
+
+  const firstnameCookie = getCookie("firstname");
+  const emailCookie = getCookie("email");
   const cookiesOutput = document.getElementById("output");
 
   const p = document.createElement("p");
-  p.innerHTML = "Cookies: ";
+  p.innerHTML = `Email: ${emailCookie} - Firstname: ${firstnameCookie}`;
 
   cookies.forEach((cookie) => {
     const [name, value] = cookie.trim().split("=");
