9015bbeaa4c7ecfec336746cf2e17e2a70f3479b
charlesnyanje <charleswilliam26@gmail.com>
2023-09-25 17:25:24 +0300

    0. Create basic cookie

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,32 +1,27 @@
-// Import the 'js-cookie' library for handling cookies
-import Cookies from 'js-cookie';
-
 function setCookies() {
-  const firstnameInput = document.getElementById('firstname').value;
-  const emailInput = document.getElementById('email').value;
+  const firstnameInput = document.getElementById('firstname');
+  const emailInput = document.getElementById('email');
 
-  // Set cookies with 'js-cookie' library
-  Cookies.set('firstname', firstnameInput);
-  Cookies.set('email', emailInput);
-
-  // Clear input fields after setting cookies
-  document.getElementById('firstname').value = '';
-  document.getElementById('email').value = '';
+  document.cookie = `firstname=${firstnameInput.value}`;
+  document.cookie = `email=${emailInput.value}`;
 }
 
 function showCookies() {
-  const outputDiv = document.getElementById('output');
-  const firstnameCookie = Cookies.get('firstname');
-  const emailCookie = Cookies.get('email');
+  const cookies = document.cookie.split(';');
+  const cookiesOutput = document.getElementById('output');
 
-  // Create a paragraph element to display cookies
-  const paragraph = document.createElement('p');
-  paragraph.innerHTML = `Cookies: First Name - ${firstnameCookie}, Email - ${emailCookie}`;
+  const p = document.createElement('p');
+  p.innerHTML = 'Cookies: ';
 
-  // Append the paragraph to the output div
-  outputDiv.appendChild(paragraph);
+  cookies.forEach((cookie) => {
+    const [name, value] = cookie.trim().split('=');
+    p.innerHTML += `${name}=${value}, `;
+  });
+
+  cookiesOutput.innerHTML = ''; 
+  cookiesOutput.appendChild(p);
 }
 
-// Export the functions for access in HTML
-window.setCookies = setCookies;
-window.showCookies = showCookies;
+// Attach event listeners to buttons
+document.getElementById('login').addEventListener('click', setCookies);
+document.getElementById('showcookies').addEventListener('click', showCookies);
--- Cookies_local_storage/webpack.config.js
+++ Cookies_local_storage/webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
   },
 };
