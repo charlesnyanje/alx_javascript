748750aa1234bd5be1e49b43a081a7ea22ec2dd8
charlesnyanje <charleswilliam26@gmail.com>
2023-09-26 08:35:58 +0300

    3. Delete cookie and mini application

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -40,6 +40,59 @@ function showCookies() {
   cookiesOutput.appendChild(p);
 }
 
+function showForm() {
+  const welcomeMessage = document.getElementById('welcome-message');
+  if (welcomeMessage) {
+    welcomeMessage.remove();
+  }
+
+  const loginForm = document.getElementById('login-form');
+  loginForm.style.display = 'block';
+}
+
+function hideForm() {
+  const loginForm = document.getElementById('login-form');
+  loginForm.style.display = 'none';
+}
+
+function deleteCookiesAndShowForm() {
+  document.cookie = 'firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  document.cookie = 'email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  showForm();
+}
+
+function showWelcomeMessageOrForm() {
+  const firstnameCookie = getCookie('firstname');
+  const welcomeMessage = document.createElement('h1');
+  welcomeMessage.id = 'welcome-message';
+
+   if (firstnameCookie) {
+    welcomeMessage.innerHTML = `Welcome ${firstnameCookie} <a href="#" id="logout-link">(logout)</a>`;
+    welcomeMessage.style.fontWeight = 'normal';
+    welcomeMessage.style.fontStyle = 'italic';
+    welcomeMessage.style.marginRight = '10px';
+
+    welcomeMessage.querySelector('#logout-link').addEventListener('click', () => {
+      deleteCookiesAndShowForm();
+      welcomeMessage.remove();
+    });
+  } else {
+    showForm();
+  }
+
+  const body = document.body;
+  //body.innerHTML = '';
+  body.appendChild(welcomeMessage); 
+}
+
 // Attach event listeners to buttons
-document.getElementById("login").addEventListener("click", setCookies);
-document.getElementById("showcookies").addEventListener("click", showCookies);
+document.getElementById('login').addEventListener('click', setCookies);
+document.getElementById('showcookies').addEventListener('click', showCookies);
+document.getElementById('login-button').addEventListener('click', () => {
+  hideForm();
+  setCookies();
+  showWelcomeMessageOrForm();
+});
+
+// Call showWelcomeMessageOrForm to initialize the page
+showWelcomeMessageOrForm();
